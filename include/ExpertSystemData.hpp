32567a2d34eefaa59e92538c8d85671fb6bcd4f7
frolushka <mpfrolovstud@gmail.com>
2021-02-18 18:36:59 +0300

    add destructor for ExpertSystemData

--- include/ExpertSystemData.hpp
+++ include/ExpertSystemData.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <functional>
+#include <unordered_set>
 
 #include "Expressions/Expression.hpp"
 #include "Expressions/FactExpression.hpp"
@@ -13,6 +15,7 @@ public:
 	~ExpertSystemData();
 	
 	void AddMainExpression(Expression *expression);
+	void AddExpression(Expression *expression);
 	
 	Expression *Find(Expression *expression) const;
 	
@@ -28,4 +31,21 @@ private:
 	std::vector<Expression *> main_expressions_;
 	std::vector<Expression *> facts_;
 	std::vector<Expression *> query_;
+
+private:
+    struct ExpressionHasher {
+    public:
+        size_t operator()(const Expression *e) const {
+            return std::hash<std::string>{}(e->ToString());
+        }
+    };
+
+    struct ExpressionComparator {
+    public:
+        size_t operator()(const Expression *lhs, const Expression *rhs) const {
+            return lhs->ToString() == rhs->ToString();
+        }
+    };
+
+	std::unordered_set<Expression *, ExpressionHasher, ExpressionComparator> unique_expressions_;
 };
