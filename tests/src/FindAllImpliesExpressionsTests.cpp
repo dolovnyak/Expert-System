e8b435debb85063939f20463a670c2e6740d8ca0
dolovnyak <dolovnyak@yandex.ru>
2021-02-19 22:45:40 +0300

    fix tests

--- tests/src/FindAllImpliesExpressionsTests.cpp
+++ tests/src/FindAllImpliesExpressionsTests.cpp
@@ -3,44 +3,45 @@
 
 class FindAllImpliesExpressionsTests : public ::testing::Test {
 public:
-	static void Assert_Eq(const std::vector<Expression *> &inputExprs, const std::vector<Expression *> &exceptExprs) {
-		ASSERT_EQ(inputExprs.size(), exceptExprs.size());
+	friend ExpertSystemData;
+	static void Assert_Eq(const std::vector<Expression *> &input_exprs, const ExpertSystemData::ExpressionsSet except_exprs) {
+		ASSERT_EQ(input_exprs.size(), except_exprs.size());
 		
-		for (int i = 0; i < inputExprs.size(); i++) {
-			ASSERT_EQ(*inputExprs[i], *exceptExprs[i]);
+		for (size_t i = 0; i < input_exprs.size(); i++) {
+			ASSERT_TRUE(except_exprs.find(input_exprs[i]) != except_exprs.end());
 		}
 	}
 };
 
 TEST_F(FindAllImpliesExpressionsTests, FindAllImpliesExpressions_Implie) {
-	ExpertSystemData data = ExpertSystem::Parse(
-			"A => C\n"
-			"A | B => M | D\n"
-			"K => A\n"
-			"D => O + (A | M)\n"
-			"J => O + (!((A)) | M)\n");
-	ExpertSystemData exceptData = ExpertSystem::Parse(
-			"K => A\n"
-			"D => O + (A | M)\n"
-			"J => O + (!((A)) | M)\n");
+	char input[1024] = "A => C\n"
+						"A | B => M | D\n"
+						"K => A\n"
+						"D => O + (A | M)\n"
+	  					"J => O + (!((A)) | M)\n";
+	ExpertSystemData *data = ExpertSystem::Parse(input);
+	char except_input[1024] = "K => A\n"
+							"D => O + (A | M)\n"
+		 					"J => O + (!((A)) | M)\n";
+	ExpertSystemData *exceptData = ExpertSystem::Parse(except_input);
 	Expression *findExpression = new FactExpression('A');
-	FindAllImpliesExpressionsTests::Assert_Eq(data.FindAllImpliesExpressions(findExpression), exceptData.GetMainExpressions());
+	FindAllImpliesExpressionsTests::Assert_Eq(data->FindAllImpliesExpressions(findExpression), exceptData->GetMainExpressions());
 }
 
 TEST_F(FindAllImpliesExpressionsTests, FindAllImpliesExpressions_Mutual_Implie) {
-	ExpertSystemData data = ExpertSystem::Parse(
-			"A <=> C\n"
-			"A | B <=> M | D\n"
-			"K + A <=> D\n"
-			"D => O + (A | M)\n"
-			"D => O + (M)\n"
-			"J => O + (!((A)) | M)\n");
-	ExpertSystemData exceptData = ExpertSystem::Parse(
-			"A <=> C\n"
-			"A | B <=> M | D\n"
-			"K + A <=> D\n"
-			"D => O + (A | M)\n"
-			"J => O + (!((A)) | M)\n");
+	char input[1024] = "A <=> C\n"
+					"A | B <=> M | D\n"
+	 				"K + A <=> D\n"
+					"D => O + (A | M)\n"
+					"D => O + (M)\n"
+					"J => O + (!((A)) | M)\n";
+	ExpertSystemData *data = ExpertSystem::Parse(input);
+	char except_input[1024] = "A <=> C\n"
+							  "A | B <=> M | D\n"
+							  "K + A <=> D\n"
+							  "D => O + (A | M)\n"
+		 					  "J => O + (!((A)) | M)\n";
+	ExpertSystemData *exceptData = ExpertSystem::Parse(except_input);
 	Expression *findExpression = new FactExpression('A');
-	FindAllImpliesExpressionsTests::Assert_Eq(data.FindAllImpliesExpressions(findExpression), exceptData.GetMainExpressions());
+	FindAllImpliesExpressionsTests::Assert_Eq(data->FindAllImpliesExpressions(findExpression), exceptData->GetMainExpressions());
 }
---
+++ tests/src/NotCorrectInputTests.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Slaughterfish Becker on 2/19/21.
+//
+
