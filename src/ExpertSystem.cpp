f4bcc83c41f5a3c210aa64ee62fd1781e505a59c
dolovnyak <dolovnyak@yandex.ru>
2021-02-24 18:29:47 +0300

    return back checker

--- src/ExpertSystem.cpp
+++ src/ExpertSystem.cpp
@@ -29,4 +29,10 @@ void ExpertSystem::Solve(ExpertSystemData &expert_system_data) {
 	for (Expression *fact : expert_system_data.GetFacts()) {
 		fact->Calculate(expert_system_data);
 	}
+	
+	//check correct
+	ExpertSystemData expert_system_data_for_check(expert_system_data);
+	for (Expression *fact : expert_system_data_for_check.GetFacts()) {
+		fact->Calculate(expert_system_data_for_check);
+	}
 }
